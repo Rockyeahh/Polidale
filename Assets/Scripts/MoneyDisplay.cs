d14086f0cba458211a69138e38484595cf7d38cb
Charlie <chaz.com@hotmail.co.uk>
2021-03-21 14:54:38 +0000

    The money text now updates on screen but it shows 0 until money is spent for the first time.

--- Assets/Scripts/MoneyDisplay.cs
+++ Assets/Scripts/MoneyDisplay.cs
@@ -7,42 +7,27 @@ using UnityEngine.UI;
 public class MoneyDisplay : MonoBehaviour
 {
     public TextAsset inkJSON;
-    //public Text moneyTextPrefab;
     Text moneyText;
-    //GameObject moneyText;
     GameController gameController;
     
     private Story story;
 
-    // Start is called before the first frame update
     void Start()
     {
         story = new Story(inkJSON.text);
-        //moneyText = GameObject.FindWithTag("MoneyText");
-        // Get gameobject component Text
-        Text moneyText = GetComponent<Text>();
 
-        GameController gameController = GameObject.Find("Canvas").GetComponent<GameController>(); // Use find with tag or something else that's faster and better than GameObject.Find
-        moneyText.text = gameController.currentMoney.ToString();
-    }
+        // DO I put this shit back in Start, rather than in Update or some other method?
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Text moneyText = Instantiate(moneyTextPrefab) as Text;
         //Text moneyText = GetComponent<Text>();
 
-        // story.ObserveVariable("starting_money", (variableName, newValue) =>
-        //  {
-        //Print the new value
-        //     Debug.Log(newValue);
-        //     moneyText.text = newValue.ToString();
-        //     moneyText.text = "Money: " + (int)newValue;
-        //  });
-
-        // IGNORE ALL OF THE above.
-        // Grab the currentMoney and ToString it to the moneyText componenent.
-
+        //GameController gameController = GameObject.Find("Canvas").GetComponent<GameController>(); // Use find with tag or something else that's faster and better than GameObject.Find
         //moneyText.text = gameController.currentMoney.ToString();
+    }
+
+    void Update()
+    {
+        Text moneyText = GetComponent<Text>();
+        GameController gameController = GameObject.FindWithTag("Canvas").GetComponent<GameController>();
+        moneyText.text = gameController.currentMoney.ToString();
     }
 }
